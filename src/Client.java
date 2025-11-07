64571d2b0b8b556e67aebd7fe02016ac82d78594
Oliwer0360 <oliwerlaulumaa@gmail.com>
2025-11-07 13:05:33 +0100

    halva klar

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,40 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
 
-public class Client{
+public class Client {
 
+    private Socket socket;
+    private BufferedReader in;
+    private BufferedWriter out;
+    private String username;
+
+    public Client(Socket socket, String username) {
+        try {
+            this.socket = socket;
+            this.in = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.out = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.username = username;
+        } catch (Exception e) {
+            closeEverything(socket, in, out);
+        }
+    }
+
+    public void sendMessage() {
+        try {
+            out.write(username);
+            out.newLine();
+            out.flush();
+
+            Scanner scan = new Scanner(System.in);
+            while (socket.isConnected()) {
+                String messageToSend = scan.nextLine();
+                out.write(username + ": " + messageToSend);
+                out.newLine();
+                out.flush();
+            }
+        } catch (Exception e) {
+            closeEverything(socket, in, out);
+        }
+    }
 }
